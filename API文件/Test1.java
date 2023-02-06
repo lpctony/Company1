77b9ee7d8bc974721db054af5c92e02c144e6720
lpctony <123169827+lpctony@users.noreply.github.com>
2023-02-06 10:23:48 +0800

    Create Test1.java

---
+++ API文件/Test1.java
@@ -0,0 +1,24 @@
+package Test_;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("test1");
+
+		System.out.println
+		("終於有些開竅了,");
+		System.out.println
+		("\t持續努力中,");
+		System.out.println
+		("\t\t有點高興了,");
+		System.out.println
+		("\t\t\t太棒了!");
+		
+		System.out.println
+		("春眠不覺曉,\t處處聞啼鳥 \n夜來風雨聲,\t花落知多少");
+		System.out.println
+		("*\n**\n***\n****\n*****");
+		
+
+	}
+}
